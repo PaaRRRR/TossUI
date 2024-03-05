139f1adb9d6e2d221581eb2c7b754a25a6d0535b
hihi1647 <hihi1647@gmail.com>
2024-03-05 22:57:06 +0900

    🤡 dummy datas

---
+++ lib/screen/main/tab/tosspay/f_tosspay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TossPay extends StatefulWidget {
+  const TossPay({super.key});
+
+  @override
+  State<TossPay> createState() => _TossPayState();
+}
+
+class _TossPayState extends State<TossPay> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
